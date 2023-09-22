6f6e2cabdb7efe3aed799976a159c937b8f3de55
Juana <jjuanazabaleta@gmail.com>
2023-09-22 14:01:01 -0300

    Ejercicio de parcial visto en clase

---
+++ Ejemplos examen/examenClase.s
@@ -0,0 +1,4 @@
+// Escribir un programa para MSXX88, utilizando el puerto PA del PIO, reciba una cadena de 5 caracteres desde un dispositivo genérico.
+//Este dispositivo envia la cadena de a un caracter a la vez. Los caracteres recibir desde el dispositivo uno por uno, esperando 10 segundos entre cada recepción.
+//El programa debe finalizar cuando se han recibido todos los caracteres de la cadena, o cuando se presiona la regla F10.
+//Una vez finalizada la lectura se debe imprimir la cadena en pantalla
